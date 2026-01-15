9cc10406d42869ab0f760fa282e37898369fd3f6
Pvrolomx <pvrolo@gmail.com>
2026-01-15 11:55:23 -0600

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'notaria-navy': '#1a365d',
+        'notaria-gold': '#d4a84b',
+        'notaria-cream': '#faf9f7',
+        'notaria-dark': '#0f172a',
+      }
+    }
+  },
+  plugins: [],
+}
